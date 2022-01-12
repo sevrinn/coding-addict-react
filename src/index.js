c8d59563ee84b661f96245830b82f9bb68d5765f
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-11 16:50:12 -0800

    added props and destructured

--- src/index.js
+++ src/index.js
@@ -2,27 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+//setup vars
+const firstBook = {
+	image:
+		'https://m.media-amazon.com/images/I/61PzAMU7SGL._AC_UY327_FMwebp_QL65_.jpg',
+	author: 'Sammie Bae',
+	title: 'JavaScript Data Structures and Algorithms',
+}
+
+const secondBook = {
+	image:
+		'https://m.media-amazon.com/images/I/91Do0MNIrxL._AC_UY327_FMwebp_QL65_.jpg',
+	author: 'Michael McMillan',
+	title: 'Data Structures and Algorithms with JavaScript',
+}
+
 const BookList = () => {
 	return (
-		<section className="bookList">
-			<Book />
-			<Book />
-			<Book />
-			<Book />
+		<section className='bookList'>
+			<Book
+				image={firstBook.image}
+				author={firstBook.author}
+				title={firstBook.title}
+			/>
+			<Book
+				image={secondBook.image}
+				author={secondBook.author}
+				title={secondBook.title}
+			/>
 		</section>
 	)
 }
-const author = 'Sammie Bae'
-const Book = () => {
-	const title = 'JavaScript Data Structures and Algorithms'
+
+const Book = ({ image, title, author }) => {
+	// const { image, title, author } = props
 	return (
-		<article className="book">
-			<img
-				src="https://m.media-amazon.com/images/I/61PzAMU7SGL._AC_UY327_FMwebp_QL65_.jpg"
-				alt=""
-			/>
+		<article className='book'>
+			<img src={image} alt='' />
 			<h2>{title}</h2>
-			<h4 className="author">{author}</h4>
+			<h4 className='author'>{author}</h4>
 		</article>
 	)
 }
