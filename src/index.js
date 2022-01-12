fc7c34c9c5fbe036cca46db3a907e363b2d03ca6
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-12 07:03:28 -0800

    added two different types of events

--- src/index.js
+++ src/index.js
@@ -4,41 +4,54 @@ import './index.css'
 
 //setup vars
 const books = [
-	{
-		id: 1,
-		image:
-			'https://m.media-amazon.com/images/I/61PzAMU7SGL._AC_UY327_FMwebp_QL65_.jpg',
-		author: 'Sammie Bae',
-		title: 'JavaScript Data Structures and Algorithms',
-	},
-	{
-		id: 2,
-		image:
-			'https://m.media-amazon.com/images/I/91Do0MNIrxL._AC_UY327_FMwebp_QL65_.jpg',
-		author: 'Michael McMillan',
-		title: 'Data Structures and Algorithms with JavaScript',
-	},
+  {
+    id: 1,
+    image:
+      'https://m.media-amazon.com/images/I/61PzAMU7SGL._AC_UY327_FMwebp_QL65_.jpg',
+    author: 'Sammie Bae',
+    title: 'JavaScript Data Structures and Algorithms',
+  },
+  {
+    id: 2,
+    image:
+      'https://m.media-amazon.com/images/I/91Do0MNIrxL._AC_UY327_FMwebp_QL65_.jpg',
+    author: 'Michael McMillan',
+    title: 'Data Structures and Algorithms with JavaScript',
+  },
 ]
 
 const BookList = () => {
-	return (
-		<section className='bookList'>
-			{books.map((book) => {
-				return <Book book={book} key={book.id}></Book>
-			})}
-		</section>
-	)
+  return (
+    <section className='bookList'>
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>
+      })}
+    </section>
+  )
 }
 
 const Book = (props) => {
-	const { image, title, author } = props.book
-	return (
-		<article className='book'>
-			<img src={image} alt='' />
-			<h2>{title}</h2>
-			<h4 className='author'>{author}</h4>
-		</article>
-	)
+  const { image, title, author } = props
+
+  //attribute, eventHandler
+  //onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e.target.innerText)
+  }
+
+  return (
+    <article className='book'>
+      <img src={image} alt='' />
+      <h2 onClick={clickHandler}>{title}</h2>
+      <h4
+        className='author'
+        onMouseOver={(e) => console.log(e.target.innerText)}
+      >
+        {author}
+      </h4>
+      <button type='button'></button>
+    </article>
+  )
 }
 
 ReactDOM.render(<BookList />, document.getElementById('root'))
