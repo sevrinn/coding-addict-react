75e4df5e7222987071c9aea75490bd3f36d816cb
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-13 05:57:31 -0800

    added another book to list

--- src/books.js
+++ src/books.js
@@ -9,8 +9,15 @@ export const books = [
   {
     id: 2,
     image:
-      'https://m.media-amazon.com/images/I/91Do0MNIrxL._AC_UY327_FMwebp_QL65_.jpg',
+      'https://images-na.ssl-images-amazon.com/images/I/51+8t5OpUyL._SX258_BO1,204,203,200_.jpg',
     author: 'Michael McMillan',
     title: 'Data Structures and Algorithms with JavaScript',
+  },
+  {
+    id: 3,
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/51OFkZ5ZYcL._SX404_BO1,204,203,200_.jpg',
+    author: 'Loiane Groner',
+    title: 'Learning JavaScript Data Structures and Algorithms',
   },
 ]
